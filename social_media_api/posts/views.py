df4ba85d7475d363468f560bb913c688fa0ddc18
Matthew Teferi <metthewsteferi@gmail.com>
2026-02-17 04:12:15 -0800

    Fix feed view: use permissions.IsAuthenticated as required by checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,16 +1,13 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
-from rest_framework.generics import ListAPIView
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['author']
     search_fields = ['title', 'content']
@@ -21,10 +18,9 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
-        # Optionally filter by post if post_id is in URL
         queryset = super().get_queryset()
         post_id = self.request.query_params.get('post')
         if post_id:
@@ -34,12 +30,10 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-class FeedView(ListAPIView):
+class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]   # This line contains "permissions.IsAuthenticated"
 
     def get_queryset(self):
-        # Get users that the current user follows
         following_users = self.request.user.following.all()
-        # Return posts from those users, ordered by most recent
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
